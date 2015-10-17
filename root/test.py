0595a8745c279518aea1df3bb307da0deea4cabd
Tom Parker <palfrey@tevp.net>
2015-10-17 15:49:10 +0100

    Tidy up test.py

--- root/test.py
+++ root/test.py
@@ -9,14 +9,9 @@ profile.set_preference("general.useragent.override","Mozilla/5.0 (iPhone; CPU iP
 driver = webdriver.Firefox(profile)
 driver.set_window_size(1024, 768)
 driver.get("http://livestream.com/accounts/4175709/tip")
-#time.sleep(2)
 element = driver.find_element_by_xpath("//div[@id='image-container']/img")
-print element.location_once_scrolled_into_view
-print element.rect
-print element.is_displayed()
-#driver.save_screenshot('screenshot.png')
 ss = driver.get_screenshot_as_png()
-im = Image.open(StringIO(ss))#"screenshot.png")
+im = Image.open(StringIO(ss))
 r = element.rect
 box = (r["x"], r["y"], r["x"] + r["width"], r["y"] + r["height"])
 cropped = im.crop(box)

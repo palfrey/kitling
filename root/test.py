1f4b971f82379d7a12b4076aae30d3070f7dd2f4
Tom Parker <palfrey@tevp.net>
2015-10-18 14:31:45 +0100

    Refactor driver setup into the StreamResource

--- root/test.py
+++ root/test.py
@@ -5,17 +5,17 @@ from PIL import Image
 from StringIO import StringIO
 import falcon
 
-profile = webdriver.FirefoxProfile()
-profile.set_preference("general.useragent.override","Mozilla/5.0 (iPhone; CPU iPhone OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) GSA/8.0.57838 Mobile/12H321 Safari/600.1.4")
-driver = webdriver.Firefox(profile)
-driver.set_window_size(1024, 768)
-
 class StreamResource:
-    def on_post(self, req, resp):
-		# "http://livestream.com/accounts/4175709/tip"
-		driver.get(req.params["url"])
-		element = driver.find_element_by_xpath("//div[@id='image-container']/img")
-		ss = driver.get_screenshot_as_png()
+	def __init__(self):
+		profile = webdriver.FirefoxProfile()
+		profile.set_preference("general.useragent.override","Mozilla/5.0 (iPhone; CPU iPhone OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) GSA/8.0.57838 Mobile/12H321 Safari/600.1.4")
+		self.driver = webdriver.Firefox(profile)
+		self.driver.set_window_size(1024, 768)
+
+	def on_post(self, req, resp):
+		self.driver.get(req.params["url"])
+		element = self.driver.find_element_by_xpath("//div[@id='image-container']/img")
+		ss = self.driver.get_screenshot_as_png()
 		im = Image.open(StringIO(ss))
 		r = element.rect
 		box = (r["x"], r["y"], r["x"] + r["width"], r["y"] + r["height"])

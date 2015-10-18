9bd48a28d0e4136dcc87bdddb29d63c0b69f859e
Tom Parker <palfrey@tevp.net>
2015-10-18 23:20:08 +0100

    Redo livestream-specific code as generic video host

--- imager/image.py
+++ imager/image.py
@@ -4,8 +4,15 @@ from os import system
 from PIL import Image
 from StringIO import StringIO
 import falcon
+import urlparse
 
 class StreamResource:
+	patterns = {
+		"livestream.com" : {
+			"path" : "//div[@id='image-container']/img"
+		}
+	}
+
 	def __init__(self):
 		profile = webdriver.FirefoxProfile()
 		profile.set_preference("general.useragent.override","Mozilla/5.0 (iPhone; CPU iPhone OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) GSA/8.0.57838 Mobile/12H321 Safari/600.1.4")
@@ -13,8 +20,16 @@ class StreamResource:
 		self.driver.set_window_size(1024, 768)
 
 	def on_post(self, req, resp):
-		self.driver.get(req.params["url"])
-		element = self.driver.find_element_by_xpath("//div[@id='image-container']/img")
+		url = req.params["url"]
+		res = urlparse.urlparse(url)
+		loc = res.netloc
+		if loc == "":
+			raise falcon.HTTPBadRequest("Bad URL", "'%s' is not a valid URL" % url)
+		if loc not in self.patterns.keys():
+			raise falcon.HTTPBadRequest("Video host %s is not supported" % loc, "Try a different host")
+		settings = self.patterns[loc]
+		self.driver.get(url)
+		element = self.driver.find_element_by_xpath(settings["path"])
 		ss = self.driver.get_screenshot_as_png()
 		im = Image.open(StringIO(ss))
 		r = element.rect

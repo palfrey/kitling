aad90c2ae954a11665e9782d7ef022b9ec79c6fc
Tom Parker <palfrey@tevp.net>
2015-10-27 22:14:21 +0000

    Run comparer through rustfmt

--- comparer/src/main.rs
+++ comparer/src/main.rs
@@ -23,25 +23,30 @@ use hyper::status::StatusCode;
 use postgres::stmt::Statement;
 
 fn prepare_statement<'a>(conn: &'a Connection, stmt: &str) -> Statement<'a> {
-	loop {
-		let prep_stmt = conn.prepare_cached(stmt);
-		if prep_stmt.is_err() {
-			error!("Error in prepare for '{}': {}", stmt, prep_stmt.unwrap_err());
-			thread::sleep_ms(4000);
-			continue;
-		}
-		return prep_stmt.unwrap();
-	}
+    loop {
+        let prep_stmt = conn.prepare_cached(stmt);
+        if prep_stmt.is_err() {
+            error!("Error in prepare for '{}': {}",
+                   stmt,
+                   prep_stmt.unwrap_err());
+            thread::sleep_ms(4000);
+            continue;
+        }
+        return prep_stmt.unwrap();
+    }
 }
 
 fn main() {
     log4rs::init_file("log.toml", Default::default()).unwrap();
     let db_url: &str = &env::var("DATABASE_URL").unwrap();
-    let conn = Connection::connect(db_url, &SslMode::None)
-                .unwrap();
+    let conn = Connection::connect(db_url, &SslMode::None).unwrap();
 
-	let query_stmt = prepare_statement(&conn, "select * from videos_video order by \"lastRetrieved\" desc limit 1");
-	let update_stmt = prepare_statement(&conn, "update videos_video set working = true, hash = $1, motion = $2, \"lastRetrieved\" = $3 where id = $4");
+    let query_stmt = prepare_statement(&conn,
+                                       "select * from videos_video order by \"lastRetrieved\" \
+                                        desc limit 1");
+    let update_stmt = prepare_statement(&conn,
+                                        "update videos_video set working = true, hash = $1, \
+                                         motion = $2, \"lastRetrieved\" = $3 where id = $4");
 
     loop {
         let res = query_stmt.query(&[]);
@@ -60,14 +65,13 @@ fn main() {
         let item = items.get(0);
         let url: String = item.get("url");
         let when: postgres::Result<Timespec> = item.get_opt("lastRetrieved");
-		let id: i32 = item.get("id");
-		let old_hash_raw: postgres::Result<String> = item.get_opt("hash");
-		let old_hash = ImageHash::from_base64(&old_hash_raw.unwrap()).unwrap();
+        let id: i32 = item.get("id");
+        let old_hash_raw: postgres::Result<String> = item.get_opt("hash");
+        let old_hash = ImageHash::from_base64(&old_hash_raw.unwrap()).unwrap();
 
         if when.is_err() {
             debug!("Item: {}, When: never", url);
-        }
-        else {
+        } else {
             let now = time::now().to_timespec();
             let diff: Duration = now - when.unwrap();
             debug!("Item: {}, When: {}", url, diff);
@@ -77,17 +81,18 @@ fn main() {
                 continue;
             }
         }
-		info!("Updating {}", url);
+        info!("Updating {}", url);
 
         let mut options = vec![];
         options.push(("url".to_string(), &url));
         let data: &str = &url::form_urlencoded::serialize(&options);
 
         let mut resp = Client::new()
-            .post("http://imager:8000/streams")
-            .header(ContentType::form_url_encoded())
-            .body(data)
-            .send().unwrap();
+                           .post("http://imager:8000/streams")
+                           .header(ContentType::form_url_encoded())
+                           .body(data)
+                           .send()
+                           .unwrap();
 
         if resp.status != StatusCode::Ok {
             warn!("{:?}", resp.status_raw());
@@ -101,13 +106,13 @@ fn main() {
         let hash = ImageHash::hash(&image, 16, HashType::DoubleGradient);
 
         debug!("Image hash: {}", hash.to_base64());
-		let motion = old_hash.dist_ratio(&hash) as f64;
-		debug!("Difference {}", motion);
-		let now = time::now().to_timespec();
-		match update_stmt.execute(&[&hash.to_base64(), &motion, &now, &id]) {
-		    Ok(_) => info!("Updated {} with motion {}", url, motion),
-		    Err(err) => warn!("Error executing update: {:?}", err)
-		}
+        let motion = old_hash.dist_ratio(&hash) as f64;
+        debug!("Difference {}", motion);
+        let now = time::now().to_timespec();
+        match update_stmt.execute(&[&hash.to_base64(), &motion, &now, &id]) {
+            Ok(_) => info!("Updated {} with motion {}", url, motion),
+            Err(err) => warn!("Error executing update: {:?}", err),
+        }
         thread::sleep_ms(4000);
     }
 }

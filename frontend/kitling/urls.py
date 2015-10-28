1ba1290951a343f563cdf35d0bc273e6ca4cd88f
Tom Parker <palfrey@tevp.net>
2015-10-28 15:02:11 +0000

    Cope with video feeds with spaces in the name

--- frontend/kitling/urls.py
+++ frontend/kitling/urls.py
@@ -11,8 +11,8 @@ router.register(r'feeds', views.FeedViewSet)
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^display/(?P<username>[\w-]+)/(?P<feedName>[\w-]+)/$', views.display),
-    url(r'^feed/(?P<username>[\w-]+)/(?P<feedName>[\w-]+)/$', views.feed),
+    url(r'^display/(?P<username>[^/]+)/(?P<feedName>[^/]+)/$', views.display),
+    url(r'^feed/(?P<username>[^/]+)/(?P<feedName>[^/]+)/$', views.feed),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),

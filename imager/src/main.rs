5ae47fc5d5bad5bf958c1c90715b40556fb784ac
Tom Parker <palfrey@tevp.net>
2016-05-20 18:00:55 +0100

    Properly extract url from args

--- imager/src/main.rs
+++ imager/src/main.rs
@@ -2,7 +2,8 @@
 extern crate log4rs;
 
 extern crate pencil;
-use pencil::{Pencil, Request, Response, PencilResult};
+use pencil::{Pencil, Request, Response, PencilResult, PenHTTPError};
+use pencil::http_errors::BadRequest;
 
 #[macro_use] extern crate hyper;
 use hyper::header::ContentLength;
@@ -15,25 +16,22 @@ use url::form_urlencoded;
 extern crate core;
 use core::ops::Deref;
 
-fn hello(request: &mut Request) -> PencilResult {
-    let length = {
-		let ref req = request.request;
-		req.headers.get::<ContentLength>().unwrap().deref();
-	};
-    println!("{:?}", length);
-    let mut buffer = String::new();
-    request.request.read_to_string(&mut buffer);
-	println!("{:?}", buffer);
+fn streams(request: &mut Request) -> PencilResult {
+	let mut buffer = String::new();
+	request.request.read_to_string(&mut buffer);
 	let mut parse = form_urlencoded::parse(buffer.as_bytes());
-	println!("{:?}", parse.find(|k| k.0 == "url"));
+	let url = match parse.find(|k| k.0 == "url") {
+		Some((_, value)) => value,
+		None => return Err(PenHTTPError(BadRequest))
+	};
     Ok(Response::from("Hello World!"))
 }
 
 fn main() {
     log4rs::init_file("log.toml", Default::default()).unwrap();
-    let mut app = Pencil::new("/web/hello");
+    let mut app = Pencil::new("");
     app.set_debug(true);
     app.set_log_level();
-    app.post("/", "hello", hello);
+    app.post("/streams", "streams", streams);
     app.run("127.0.0.1:8000");
 }
